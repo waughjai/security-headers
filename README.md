bce1b9bae23664fa7c690b897a461517df3b8835
Jaimeson Waugh <jaimeson@4cesi.com>
2019-12-02 10:52:14 -0800

    Clean up code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Security Headers
 =========================
 
-Simple class for applying security headers to website. Just call `SecurityHeaders::setAllAutoPolicies()`.
+Simple class for applying security headers to website.
+
+Just call `SecurityHeaders::setAllAutoPolicies()` before rendering anything.
--- src/SecurityHeaders.php
+++ src/SecurityHeaders.php
@@ -1,42 +1,41 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\SecurityHeaders
+namespace WaughJ\SecurityHeaders;
+
+class SecurityHeaders
 {
-	class SecurityHeaders
+	public static function setAllAutoPolicies() : void
 	{
-		public static function setAllAutoPolicies()
-		{
-			self::setSecureXFrameOption();
-			self::setXSSProtectionOn();
-			self::setNoSniffOn();
-			self::setSecureReferrerOn();
-			self::setHSTS();
-		}
+		self::setSecureXFrameOption();
+		self::setXSSProtectionOn();
+		self::setNoSniffOn();
+		self::setSecureReferrerOn();
+		self::setHSTS();
+	}
 
-		public static function setHSTS()
-		{
-			header( 'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload; env=HTTPS' );
-		}
+	public static function setHSTS() : void
+	{
+		header( 'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload; env=HTTPS' );
+	}
 
-		public static function setSecureXFrameOption()
-		{
-			header( 'X-Frame-Options: SAMEORIGIN' );
-		}
+	public static function setSecureXFrameOption() : void
+	{
+		header( 'X-Frame-Options: SAMEORIGIN' );
+	}
 
-		public static function setXSSProtectionOn()
-		{
-			header( 'X-Xss-Protection: 1; mode=block' );
-		}
+	public static function setXSSProtectionOn() : void
+	{
+		header( 'X-Xss-Protection: 1; mode=block' );
+	}
 
-		public static function setNoSniffOn()
-		{
-			header( 'X-Content-Type-Options: nosniff' );
-		}
+	public static function setNoSniffOn() : void
+	{
+		header( 'X-Content-Type-Options: nosniff' );
+	}
 
-		public static function setSecureReferrerOn()
-		{
-			header( 'Referrer-Policy: no-referrer-when-downgrade' );
-		}
+	public static function setSecureReferrerOn() : void
+	{
+		header( 'Referrer-Policy: no-referrer-when-downgrade' );
 	}
 }
